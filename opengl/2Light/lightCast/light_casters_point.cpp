6f92315ccd57976d03bf16443e61c99f329ea193
bootsyang <2338109332@qq.com>
2024-09-15 14:23:02 +0800

    2.4.lightcast

--- opengl/2Light/lightCast/light_casters_point.cpp
+++ opengl/2Light/lightCast/light_casters_point.cpp
@@ -78,7 +78,7 @@ int main()
 
     // build and compile our shader program
     // ------------------------------------
-    Shader ourShader("../opengl/Shader/shaders/241colorObject.vs", "../opengl/Shader/shaders/241colorObject.fs");
+    Shader ourShader("../opengl/Shader/shaders/241colorObject.vs", "../opengl/Shader/shaders/252colorObject.fs");
     Shader lightShader("../opengl/Shader/shaders/211colorLight.vs", "../opengl/Shader/shaders/211colorLight.fs");
 
     stbi_set_flip_vertically_on_load(true);
@@ -128,6 +128,19 @@ int main()
         -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
     };
+
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
     // first, configure the cube's VAO (and VBO)
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -162,6 +175,9 @@ int main()
     ourShader.use();
     ourShader.setInt("material.diffuse", 0);
     ourShader.setInt("material.specular", 1);
+    ourShader.setFloat("light.constant",  1.0f);
+    ourShader.setFloat("light.linear",    0.09f);
+    ourShader.setFloat("light.quadratic", 0.032f);
 
     // render loop
     // -----------
@@ -204,18 +220,25 @@ int main()
         // world transformation
         glm::mat4 model = glm::mat4(1.0f);
         ourShader.setMat4("model", model);
-
+        // render the cube
+        glBindVertexArray(VAO);
         // bind diffuse map
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, diffuseMap);
         // bind specular map
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, specularMap);
+        ourShader.setVec3("light.direction", 0.2f, -1.0f, -0.3f);
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);
 
-        // render the cube
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         // also draw the lamp object
         lightShader.use();
